31a4ceda11693c18a66984915372ab6c0f6e407e
ipekmuhammet <muhammetipek57@hotmail.com>
2020-05-11 06:10:20 +0300

    .

--- ios/platformappTests/platformappTests.m
+++ ios/platformappTests/platformappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface platformappTests : XCTestCase
+@interface bloomTests : XCTestCase
 
 @end
 
-@implementation platformappTests
+@implementation bloomTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
