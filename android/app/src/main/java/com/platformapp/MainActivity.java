28c1a5d76e6a6ab8a80202435eeebd14b50ba4de
ipekmuhammet <muhammetipek57@hotmail.com>
2020-05-17 19:07:05 +0300

    bloom to silfavell

--- android/app/src/main/java/com/platformapp/MainActivity.java
+++ android/app/src/main/java/com/platformapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.bloom;
+package com.silfavell.android;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "bloom";
+    return "silfavell";
   }
 }
--- ios/platformappTests/platformappTests.m
+++ ios/platformappTests/platformappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface bloomTests : XCTestCase
+@interface silfavellTests : XCTestCase
 
 @end
 
-@implementation bloomTests
+@implementation silfavellTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
