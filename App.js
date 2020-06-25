7564bb3486e2676fa4477752952f3043b0b16709
ipekmuhammet <muhammetipek57@hotmail.com>
2020-06-25 06:51:19 +0300

    .

--- App.js
+++ App.js
@@ -55,7 +55,7 @@ export default function App() {
 
 	return (
 		<Provider store={store}>
-			<StatusBar backgroundColor='black' barStyle='light-content' />
+			<StatusBar backgroundColor='rgba(0,0,0,.8)' barStyle='light-content' />
 			<GlobalScreen />
 			<NavigationContainer>
 				<Stack.Navigator initialRouteName='Loading'>
