03ddb5ef6e4116c641a59c46b9b5c9d696d3d5e7
ipekmuhammet <muhammetipek57@hotmail.com>
2020-07-16 11:22:13 +0300

    .

--- utils/global.js
+++ utils/global.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export, no-undef
-export const SERVER_URL = __DEV__ ? 'http://192.168.1.101:3000' : 'http://ip172-18-0-15-bs7js73oudsg00c5iea0-3000.direct.labs.play-with-docker.com'
+export const SERVER_URL = __DEV__ ? 'http://192.168.1.101:3000' : 'http://142.93.133.9:3000'
