4236421e0c4e043b5e163141034b6a45ab9fcb29
ipekmuhammet <muhammetipek57@hotmail.com>
2020-10-12 00:31:51 +0300

    disable proguard

--- index.js
+++ index.js
@@ -6,8 +6,4 @@ import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
 
-import Config from 'react-native-config'
-
-alert('---' + ' ' + Config.SERVER_URL + ' ' + __DEV__)
-
 AppRegistry.registerComponent(appName, () => App)
