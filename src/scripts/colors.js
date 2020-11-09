ecfc9657a65bae0113fb32986660aeed985c10e2
Muhammet İpek <muhammetipek57@hotmail.com>
2020-11-09 19:37:32 +0300

    extra colors

--- src/scripts/colors.js
+++ src/scripts/colors.js
@@ -1,5 +1,9 @@
+// eslint-disable-next-line import/prefer-default-export
 export const COLORS = {
     PRIMARY: '#4E4063',
     SECONDARY: '#6B5988',
-    TERTIARY: '#AA9BBF'
-  }
+    TERTIARY: '#AA9BBF',
+    DARK: '#000',
+    LIGHT: '#FFF',
+    GRAY: '#CDCDCD'
+}
