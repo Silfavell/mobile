59207b3f54c040005f9eb61c663212f880eddc3c
Muhammet İpek <muhammetipek57@hotmail.com>
2020-12-06 16:09:27 +0300

    update colors & disable makeOrder request

--- src/scripts/colors.js
+++ src/scripts/colors.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/prefer-default-export
 export const COLORS = {
-    PRIMARY: '#4E4063',
-    SECONDARY: '#6B5988',
-    TERTIARY: '#AA9BBF',
+    PRIMARY: '#004D40',
+    SECONDARY: '#00695C',
+    TERTIARY: '#00796B',
     DARK: '#000000',
     LIGHT: '#FFFFFF',
     GRAY: '#CDCDCD',
